182ca128d0aee0c6a568ebc8055e32ea9b2dbee2
Petra Barancikova <Pida@Petras-iMac.local>
2020-06-23 21:54:27 +0200

    init

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+
+setup(
+    name='costra',
+    version='0.1dev',
+    author='Petra Barancikova',
+    author_email='barancikova@ufal.mff.cuni.cz',
+    url='https://github.com/barancik/costra',
+    packages=['costra'],
+    package_data={'costra':['data/data.tsv']},
+    include_package_data=True,
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    keywords='embeddings evaluation',
+
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. See
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires='>=3.5, <4',
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy'],  # Optional
+
+
+
+
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
+    #
+
+)
